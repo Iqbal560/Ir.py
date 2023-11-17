48da747153a40c093f5c22f36fe2fbbafff0f1dd
Iqbal560 <144352311+Iqbal560@users.noreply.github.com>
2023-11-16 16:27:14 -0800

    Update streamlit_Ir.py

--- streamlit_Ir.py
+++ streamlit_Ir.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain .llms import OpenAI
 st.title('Quickstart App ChatGpt')
 openai_api_key=st.sidebar.text_input('OpenAI API Key')
-#openai_api_key="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"
+
 def generate_response(text_input):
 	llm=OpenAI(temperature=0.7,openai_api_key=openai_api_key)
 	st.info(llm(text_input))
